baafdcd3c2a5a53bfa2c7027321745193ef7cb56
Ilias Bikenov <ilias_bikenov@epam.com>
2022-10-31 11:40:32 +0600

    task-3-4

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import axios, { AxiosError } from 'axios';
 
 type CSVFileImportProps = {
   url: string;
@@ -23,24 +24,26 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   };
 
   const uploadFile = async () => {
-    console.log("uploadFile to", url);
+    console.log('uploadFile to', url);
 
     // Get the presigned URL
-    // const response = await axios({
-    //   method: "GET",
-    //   url,
-    //   params: {
-    //     name: encodeURIComponent(file.name),
-    //   },
-    // });
-    // console.log("File to upload: ", file.name);
-    // console.log("Uploading to: ", response.data);
-    // const result = await fetch(response.data, {
-    //   method: "PUT",
-    //   body: file,
-    // });
-    // console.log("Result: ", result);
-    // setFile("");
+    const response: { data: { signedUrl: string } } = await axios({
+      method: 'GET',
+      url,
+      params: {
+        name: encodeURIComponent(file?.name || ''),
+      },
+    });
+
+    console.log('File to upload: ', file?.name);
+    console.log('Uploading to: ', response.data.signedUrl);
+    const result = await fetch(response.data.signedUrl, {
+      method: 'PUT',
+      body: file,
+      headers: new Headers({ 'content-type': '' }),
+    });
+    console.log('Result: ', result);
+    setFile(undefined);
   };
   return (
     <Box>
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,10 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product:
+    'https://h1l7nk2vf9.execute-api.us-east-1.amazonaws.com/dev/products',
+  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://n4aiidpojf.execute-api.us-east-1.amazonaws.com/dev',
+  bff: 'https://h1l7nk2vf9.execute-api.us-east-1.amazonaws.com/dev/products',
+  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
