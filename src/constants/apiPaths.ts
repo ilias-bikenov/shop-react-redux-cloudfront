0c66c2cf2dcb2204a1505c37801238e5c93a1efd
ilias-bikenov <ilias.bikenov@gmail.com>
2022-11-11 15:48:20 +0600

    task-7

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product:
-    'https://h1l7nk2vf9.execute-api.us-east-1.amazonaws.com/dev/products',
+    'https://70twners13.execute-api.us-east-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://n4aiidpojf.execute-api.us-east-1.amazonaws.com/dev',
-  bff: 'https://h1l7nk2vf9.execute-api.us-east-1.amazonaws.com/dev/products',
+  bff: 'https://70twners13.execute-api.us-east-1.amazonaws.com/dev/products',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
